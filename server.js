212a4671732b402da19778e6a4b1026d34d7e8fa
OscarDHdz <oscardavid.hernandez.mx@gmail.com>
2020-09-05 14:42:26 -0500

    Create todo router

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+//////////////////////////////////////////////////
+// Routers ///////////////////////////////////////
+//////////////////////////////////////////////////
+const todoRouter = require('./routes/todo');
+app.use('/api', todoRouter);
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
